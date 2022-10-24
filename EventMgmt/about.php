94144596a03287816b5682ae4b2f79e1cf34bcdd
ManeeshAucharla953 <107507211+ManeeshAucharla953@users.noreply.github.com>
2022-10-24 16:47:56 -0500

    Add files via upload

---
+++ EventMgmt/about.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Event Management Systems</title>
+        <link rel = "stylesheet" type = "text/css" href = "css/style.css">
+        <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
+    </head>
+    <body>
+        <?php require 'utils/header.php'; ?>
+        <div class = "content">
+            <div class = "container">
+                    <h1>Meet the Team</h1>
+            </div>
+			
+            <div class="container">
+            <hr>
+            </div>
+            
+            <div class="containerForm">
+                    <h1> My Details: </h1>
+         
+                    <p>
+                         Name: Maneesh Aucharla<br>
+                         Mobile: 9842913021<br>
+                         Email: mxa15640@gmail.com<br>
+                         Position: Graduate Assistant
+                    </p>
+                    
+            </div>
+			
+        </div>
+        <?php require 'utils/footer.php'; ?>
+    </body>
+</html>
